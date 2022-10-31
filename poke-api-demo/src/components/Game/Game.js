788595c15c9a74e5658e2cabe0137a1781990232
kenan aljaber <kenan.aljaber.p@gmail.com>
2022-10-31 00:27:45 -0500

    adding Game component to make a simple game

--- poke-api-demo/src/components/Game/Game.js
+++ poke-api-demo/src/components/Game/Game.js
@@ -3,7 +3,7 @@ import GameCard from "./GameCard";
 import { useEffect, useState } from "react";
 import axios from "axios";
 const Game = () => {
-    const gameCardgeneratorLink = "http://pokemon-api-challange.herokuapp.com//api-kruger/pokemon/game";
+    const gameCardgeneratorLink = "http://pokemon-api-challange.herokuapp.com/api-kruger/pokemon/game";
     const [gameCard, setGameCard] = useState(undefined);
     const fetchGameCard = async () => {
         const response = await axios.get(gameCardgeneratorLink);
